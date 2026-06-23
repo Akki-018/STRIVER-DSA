4687ae40221d4f6fb932cc036c5a2ff524ce12df
Akshat <akshatkashyap135@gmail.com>
2026-06-23 23:14:55 +0530

    Solved Search_in_2d_matrix brute solution

---
+++ 9_BINARY_SEARCH_3/q3_search_in_2d_matr_2.py
@@ -0,0 +1,13 @@
+## Search in a matrix a targeted element -- but the matrix is ascending from left to right , increases from top to bottom 
+
+# BRUTE FORCE - TC - o(m*n), SC- o(1)
+def search_2d_2_brute(matrix,target):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == target:
+                return True
+    return False
+matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+print(search_2d_2_brute(matrix,5))
