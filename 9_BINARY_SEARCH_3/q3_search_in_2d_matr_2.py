65c77719fd9fad0e2995216e65cb173ed86e5419
Akshat <akshatkashyap135@gmail.com>
2026-06-24 14:01:51 +0530

    Solved Search in a 2d matrix_2 by optimal approach

--- 9_BINARY_SEARCH_3/q3_search_in_2d_matr_2.py
+++ 9_BINARY_SEARCH_3/q3_search_in_2d_matr_2.py
@@ -11,3 +11,22 @@ def search_2d_2_brute(matrix,target):
     return False
 matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
 print(search_2d_2_brute(matrix,5))
+
+## OPTIMAL SOLUTION -- BINARY SEARCH - TC - O(m*log(n)), SC-o(1)
+def search_2d_2_opt(matrix,target):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    for i in range(rows):
+        low = 0
+        high = cols-1
+        while low<=high:
+            mid = (low+high)//2
+            if matrix[i][mid] == target:
+                return True
+            elif matrix[i][mid]<target:
+                low = mid+1
+            else:
+                high = mid-1
+    return False
+matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+print(search_2d_2_opt(matrix,122))
