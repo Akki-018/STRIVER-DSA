6e998e4e60c510dc1e600933fdfd48a3e33dce07
Akshat <akshatkashyap135@gmail.com>
2026-06-25 17:17:16 +0530

    Solved Longest common prefix by brute approach

---
+++ 10_STRINGS_LEVEL-1/q4_longest_common_prefix.py
@@ -0,0 +1,14 @@
+## RETURN THE LONGEST COMMON PREFIX
+# BRUTE FORCE 
+def longest_comm_prefix(strs):
+    a = ""
+    min_len = len(strs[0])
+    for i in strs:
+        min_len = min(min_len,len(i))
+    for i in range(min_len):
+        for j in range(1,len(strs)):
+            if strs[j][i]!=strs[0][i]:
+                return a 
+        a += strs[0][i]
+    return a
+print(longest_comm_prefix(["flower","fl","flag"]))
