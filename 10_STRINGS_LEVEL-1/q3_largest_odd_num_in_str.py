695bf2834e7bbd117093b826499e7622830654aa
Akshat <akshatkashyap135@gmail.com>
2026-06-25 15:58:05 +0530

    Solved Largest_odd_num_in_a_string by optimal approach

--- 10_STRINGS_LEVEL-1/q3_largest_odd_num_in_str.py
+++ 10_STRINGS_LEVEL-1/q3_largest_odd_num_in_str.py
@@ -1,6 +1,6 @@
 ## RETURN THE LARGEST ODD NUMBER STRING IN A STRING 
 # BRUTE FORCE -- TC - O(n^2)
-def largest_odd_str(num):
+def largest_odd_str_brute(num):
     if int(num)%2==1 :
         return num
     maxi = float('-inf')
@@ -14,5 +14,15 @@ def largest_odd_str(num):
         return ""
     else:
         return str(maxi)
-print(largest_odd_str(str(24)))
+print(largest_odd_str_brute(str(24)))
+
+
+## OPTIMAL SOLUTION 
+def largest_odd_str_opt(num):
+    for i in range(len(num)-1,-1,-1):
+        if int(num[i])%2==1:
+            return num[:i+1]
+    return ""
+print(largest_odd_str_opt(str(24352)))
+
  
