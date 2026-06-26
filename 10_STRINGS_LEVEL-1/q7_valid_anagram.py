f1f5ecbeaec0a7424bdf84daf07bc0b4005150ca
Akshat <akshatkashyap135@gmail.com>
2026-06-26 16:44:23 +0530

    Solved Valid anagram by two approaches - better and one linear

--- 10_STRINGS_LEVEL-1/q7_valid_anagram.py
+++ 10_STRINGS_LEVEL-1/q7_valid_anagram.py
@@ -1,5 +1,5 @@
 ## RETURN TRUE IF THE GIVEN STRING IS ANAGRAM OF THE PREVIOUS STRING 
-# Brute Force
+# Brute Force - TC - O(n),SC - o(n)
 def valid_anagram_brute(s,t):
     if len(s)!=len(t):
         return False
@@ -10,7 +10,29 @@ def valid_anagram_brute(s,t):
         freq2[t[i]] = freq2.get(t[i],0)+1
     return freq1==freq2
 s = "anagram"
-t = "ganarim"
+t = "ganarm"
 print(valid_anagram_brute(s,t))
 
-    
+# BETTER APPROACH -
+def valid_anagram_bett(s,t):
+    if len(s)!=len(t):
+        return False
+    freq = {}
+    for i in range(len(s)):
+        freq[s[i]] = freq.get(s[i],0)+1
+    for i in range(len(s)):
+        if t[i] not in freq:
+            return False
+        freq[t[i]]-=1
+    for value in freq.values():
+        if value!=0:
+            return False
+    return True
+s = "akshat"
+t = "kshati"
+print(valid_anagram_bett(s,t))
+
+## ONE LINER APPROACH 
+def valid_anagram(s,t):
+    return sorted(s.lower())==sorted(t.lower())
+print(valid_anagram(s,t))
