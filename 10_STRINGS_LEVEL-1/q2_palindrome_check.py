863be8043a81dcaf648ee98960346a906ca2feb7
Akshat <akshatkashyap135@gmail.com>
2026-06-29 13:15:05 +0530

    Solved Palindrome checking by optimal solution

--- 10_STRINGS_LEVEL-1/q2_palindrome_check.py
+++ 10_STRINGS_LEVEL-1/q2_palindrome_check.py
@@ -9,6 +9,21 @@ def palindrome(x):
 print(palindrome(1011))
 
 ## CHECK IF A string IS PALINDROME OR NOT 
+#Direct method
 def palindrome_str(x):
     return x == x[::-1]
-print(palindrome_str("ab"))
+print(palindrome_str("aba"))
+
+#TWO POINTER APPROACH 
+def palindrome_str_opt(x):
+    low = 0 
+    high = len(x)-1
+    for i in range(len(x)):
+        if x[low]!=x[high]:
+            return False
+        low+=1
+        high-=1
+    return True
+print(palindrome_str_opt("abab"))
+
+
