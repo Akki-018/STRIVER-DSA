18be258aa2aaabd4d9d0127ff4eafaebd811cb7f
Akshat <akshatkashyap135@gmail.com>
2026-07-01 16:48:11 +0530

    Solved String to integer _ optimal approach

--- 10_STRINGS_LEVEL-2/q4_string_to_integer(atoi).py
+++ 10_STRINGS_LEVEL-2/q4_string_to_integer(atoi).py
@@ -22,3 +22,26 @@ def string_to_int(s):
 
 s = " -042"
 print(string_to_int(s))
+
+# OPTIMAL CODE 
+def string_to_int_opt(s):
+    num = 0 
+    sign = 1
+    i = 0 
+    while i<len(s) and s[i]==" ":
+        i+=1
+    if i<len(s) and (s[i]=="-" or s[i]=="+"):
+        if s[i]=='-':
+            sign = -1
+        i+=1
+    while i<len(s) and s[i].isdigit():
+        digit = ord(s[i])-ord('0') 
+        if num>(2**31-1)//10 or (num==(2**31-1)//10 and digit>7):
+            return 2**31-1 if sign == 1 else -2**31
+        num = num*10+digit
+        i+=1
+    return sign*num
+s = " -042"
+print(string_to_int_opt(s))
+
+    
