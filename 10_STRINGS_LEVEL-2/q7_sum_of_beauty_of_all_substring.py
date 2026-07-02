a1f93734f60feac3ba64c54c848c045c86643647
Akshat <akshatkashyap135@gmail.com>
2026-07-02 17:14:40 +0530

    Solved Sum of all beauty of all substrings by brute force

---
+++ 10_STRINGS_LEVEL-2/q7_sum_of_beauty_of_all_substring.py
@@ -0,0 +1,14 @@
+## RETURN THE SUM OF BEAUTY OF ALL SUBSTRINGS
+# BRUTE FORCE 
+def sum_beauty_substr_brute(s):
+    summ = 0 
+    for i in range(len(s)):
+        for j in range(i,len(s)):
+            freq = {}
+            a = s[i:j+1]
+            for m in a:
+                freq[m] = freq.get(m,0)+1
+            summ+=max(freq.values())-min(freq.values())
+    return summ 
+s = "aabcbaa"
+print(sum_beauty_substr_brute(s))
