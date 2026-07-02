543f2a27b8c2e9840993d7ca8ca9776faf53b735
Akshat <akshatkashyap135@gmail.com>
2026-07-02 17:04:53 +0530

    Solved Longest palindromic substring by optimal solution

--- 10_STRINGS_LEVEL-2/q6_longest_palindromic_substring.py
+++ 10_STRINGS_LEVEL-2/q6_longest_palindromic_substring.py
@@ -13,3 +13,23 @@ def longest_palindromic_substr_brute(s):
     return ans 
 s = "babad"
 print(longest_palindromic_substr_brute(s))
+
+# OPTIMAL CODE 
+def longest_palind_substr_opt(s):
+    def expand(left,right):
+        while left>=0 and right<len(s) and s[left]==s[right]:
+            left-=1
+            right+=1
+        return right-left-1
+    start = 0
+    end = 0 
+    for i in range(len(s)):
+        odd = expand(i,i)
+        even = expand(i,i+1)
+        length = max(odd,even)
+        if length>end-start+1:
+            start = i-(length-1)//2
+            end = i+length//2
+    return s[start:end+1]
+s = "babad"
+print(longest_palind_substr_opt(s))
