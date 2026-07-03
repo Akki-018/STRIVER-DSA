cb4613065cfe0402c1f663e0518bb815d76fec98
Akshat <akshatkashyap135@gmail.com>
2026-07-03 18:21:31 +0530

    Created functions for the traversal and counting of nodes in a Singly linked list

--- 11_LINKED_LIST_1D.py/p2_traversal_in_ll.py
+++ 11_LINKED_LIST_1D.py/p2_traversal_in_ll.py
@@ -8,11 +8,28 @@ n1 = Node(5)
 n2 = Node(10)
 n3 = Node(15)
 n4 = Node(20)
-head  = n1
+head = n1
 n1.next = n2
 n2.next = n3 
 n3.next = n4
-current = head 
-while current:
-    print(current.data)
-    current = current.next
+def print_ll(head):
+    current = head
+    while current:
+        print(current.data)
+        current = current.next
+print_ll(head)
+
+def count_node_ll(head):
+    current = head
+    cnt=0
+    while current:
+        cnt+=1
+        current = current.next
+    return f"Number of nodes in the linked list: {cnt}" 
+print(count_node_ll(head))
+
+
+
+
+
+
