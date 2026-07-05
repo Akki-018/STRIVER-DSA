f39a2be5bdc1f42ab0f36a6a89f825745ca3a598
Akshat <akshatkashyap135@gmail.com>
2026-07-05 14:48:54 +0530

    Singly linked list - basics, and various operations

--- 11_LINKED_LIST_1D.py/p1_basic_ll.py
+++ 11_LINKED_LIST_1D.py/p1_basic_ll.py
@@ -1,22 +1,17 @@
-## Linked list stores element in nodes - every node has data and next - where next stores the address of the next element 
-# SINGLY LINKED LIST 
-class Node:                   # Creating own datatype -  Node 
-    def __init__(self,data):    # self - refers to the particular object 
+### Linked list stores element in nodes - every node has data and next - where next stores the address of the next element 
+## SINGLY LINKED LIST 
+# CREATING A SINGLY LINKED LIST 
+class Node:
+    def __init__(self,data):
         self.data = data
-        self.next = None           
+        self.next = None
 n1 = Node(5)
-n2 = Node(15)
-n3 = Node(25) 
-n4 = Node(35)
-head = n1
-n1.next = n2
-n2.next = n3
-n3.next = n4 
-n2.data = 50 
-print(head.next.data)
+n2 = Node(10)
+n3 = Node(15)
+n4 = Node(20)
+n5 = Node(25)
 
-n5 = Node(40)
-n4.next = n5
+
 
 
 
