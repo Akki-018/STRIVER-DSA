16862d987aa99c7dc7d930ec32f28308ae9e9610
Akshat <akshatkashyap135@gmail.com>
2026-07-22 13:43:14 +0530

    Solved Sort_list by optimal and brute force approach

--- 12_LINKED_LIST_MED_SLL.py/q10_sort_list.py
+++ 12_LINKED_LIST_MED_SLL.py/q10_sort_list.py
@@ -44,3 +44,50 @@ def sort_list_brute(head):
     return head
 head = sort_list_brute(head)
 traversal_sll(head)
+
+## OPTIMAL CODE - TC - o(nlogn),SC -o(1)
+def sort_list_opt(head):
+    if not head or not head.next:
+        return head
+    def mid_head(head):
+        if not head or not head.next:
+            return head 
+        slow = head 
+        fast = head
+        prev = None 
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        return prev 
+    mid = mid_head(head)
+    right = mid.next
+    mid.next = None
+    left = head 
+
+    left_list = sort_list_opt(left)
+    right_list = sort_list_opt(right)
+
+    def merge_sort(left,right):
+        dummy = Node(-1)
+        tail = dummy
+        curr1 = left
+        curr2 = right
+        while curr1 and curr2:
+            if curr1.data<=curr2.data:
+                tail.next = curr1
+                curr1 = curr1.next
+            else:
+                tail.next = curr2
+                curr2 = curr2.next
+            tail = tail.next 
+        if curr1:
+            tail.next = curr1
+        else:
+            tail.next = curr2
+        return dummy.next 
+    
+    return merge_sort(left_list,right_list)
+head = sort_list_opt(head)
+traversal_sll(head)
+        
