6da34aac41bd0f3545185b9f7e0d97fc1880bffa
Akshat <akshatkashyap135@gmail.com>
2026-07-27 20:52:34 +0530

    Count number of set_bits

---
+++ 14_BIT_MANIPULATION_1.py/06_count_number_of_set_bits.py
@@ -0,0 +1,18 @@
+## TO COUNT THE NUMBER OF SET BITS IN A BINARY REPRESENTATION OF A NUMBER 
+# BRUTE FORCE CODE  - TC - o(log2n),SC-o(1)
+def count_number_set_bits(n): 
+    cnt = 0 
+    while n>0:
+        cnt+=(n&1)
+        n=n>>1
+    return cnt 
+print(count_number_set_bits(40))
+
+# Optimal approach - Brian Kernighian Algo 
+def count_number_set_bit_opt(n):
+    cnt = 0 
+    while n>0:
+        cnt+=1
+        n=n&(n-1)
+    return cnt 
+print(count_number_set_bit_opt(40))
