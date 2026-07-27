522421eaac01e99df7904ff59b8069f21ffcc10b
Akshat <akshatkashyap135@gmail.com>
2026-07-27 15:55:45 +0530

    BIT MANIPULATION - Basics

---
+++ 14_BIT_MANIPULATION_1.py/01_intro_to_bits.py
@@ -0,0 +1,23 @@
+## A bit is the smallest unit of data that a computer can store - can have only two possible values - 0 or 1 
+# Computers uses binary only because it is built of electronic ckts(transistors) and it has only two stable states i.e Off - 0 or ON -1 
+# DECIMAL NUMBER SYSTEM - Base 10 - 0 1 2 3 4 5 6 7 8 9 
+# BINARY NUMBER SYSTEM - Base 2 - 0 1 
+# BINARY TO DECIMAL 
+def binary2decimal(s):
+    s = s[::-1]
+    n = len(s)
+    ans = 0
+    for i in range(n):
+        ans += int(s[i])*(2**(i))
+    return ans
+print(binary2decimal("1101"))
+
+# DECIMAL TO BINARY 
+def decimal2binary(n):
+    ans = ""
+    while n!=0 :
+        ans+=str(n%2)
+        n = n//2
+    return ans[::-1]
+print(decimal2binary(13))
+
