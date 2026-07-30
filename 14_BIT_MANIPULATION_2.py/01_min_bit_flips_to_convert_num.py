f47e8dc76bddd8c1dd96e91d5e29755a547c451f
Akshat <akshatkashyap135@gmail.com>
2026-07-30 12:58:45 +0530

    Minimum bits flip to convert a number by brute method

---
+++ 14_BIT_MANIPULATION_2.py/01_min_bit_flips_to_convert_num.py
@@ -0,0 +1,16 @@
+## Minimum bits flips to convert a number 
+
+# BRUTE FORCE - Without bit manipulation 
+def min_bits_flip_brut(start,goal):
+    start_bin = bin(start)[2:]
+    goal_bin = bin(goal)[2:]
+
+    leng = max(len(start_bin),len(goal_bin))
+    start_bin = start_bin.zfill(leng)
+    goal_bin = goal_bin.zfill(leng)
+    cnt = 0 
+    for i in range(leng):
+        if start_bin[i]!=goal_bin[i]:
+            cnt+=1
+    return cnt 
+print(min_bits_flip_brut(10,7))
