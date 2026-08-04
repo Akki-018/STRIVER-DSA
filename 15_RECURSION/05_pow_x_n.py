6a72c9750e615a41052dca507ee00f3498b54d9c
Akshat <akshatkashyap135@gmail.com>
2026-08-04 20:28:10 +0530

    Pow_x_n

--- 15_RECURSION/05_pow_x_n.py
+++ 15_RECURSION/05_pow_x_n.py
@@ -1,7 +1,14 @@
 ## Return the x**n
 def pow_x_n(x,n):
+    if n<0:
+        return 1/pow_x_n(x,-n)
     if n==0:
         return 1 
-    return pow_x_n(x,n//2)**pow_x_n(x,n//2)
+    half = pow_x_n(x,n//2)
+    if n%2==0:
+        return half*half
+    else:
+        return x*half*half
 print(pow_x_n(2,10))
+
 
